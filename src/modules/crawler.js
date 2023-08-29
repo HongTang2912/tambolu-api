8ed92e2e6372553e86ea3e928481029983660017
hongtang-digiex <hongtang240@gmail.com>
2023-08-29 17:30:00 +0700

    29/08/2023 17:29

--- src/modules/crawler.js
+++ src/modules/crawler.js
@@ -59,6 +59,36 @@ class Browser {
     return data;
   };
 
+  downloadImage = async (url, filename) => {
+    // Launch a browser instance (non-headless)
+    const browser = await puppeteer.launch();
+
+    // Open a new page
+    const page = await browser.newPage();
+
+    // Navigate to a URL with the image
+    const imageUrl = url;
+    await page.goto(imageUrl);
+
+    // Get the image's response
+    const imageResponse = await page.goto(imageUrl);
+
+    // Extract the image's content and file extension
+    const imageContent = await imageResponse.buffer();
+    const fileExtension = path.extname(imageUrl);
+
+    // Specify the destination path to save the image
+    const imagePath = path.join(__dirname, filename + fileExtension);
+
+    // Save the image to the specified path
+    fs.writeFileSync(imagePath, imageContent);
+
+    console.log("Image downloaded successfully:", imagePath);
+
+    // Close the browser
+    await browser.close();
+  };
+
   closeBrowser = async () => {
     // Close the browser
     await this.browser.close();
