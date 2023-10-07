2512f917af10574ded9349a522ba0de624360f69
hongtang-digiex <hongtang240@gmail.com>
2023-10-07 09:14:39 +0700

    pull

--- server2.js
+++ server2.js
@@ -1,9 +1,10 @@
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const neo4j = require("neo4j-driver");
 const { gql, ApolloServer } = require("apollo-server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 require("dotenv").config();
 
-const typeDefs = `
+const typeDefs = `#graphql
   type Category {
     name: String!
     url: String!
@@ -19,6 +20,8 @@ const typeDefs = `
     image_url: String
     category: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
   }
+
+
 `;
 
 const driver = neo4j.driver(
@@ -26,14 +29,22 @@ const driver = neo4j.driver(
   neo4j.auth.basic(process.env.DB_USERNAME, process.env.DB_PASSWORD)
 );
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-
-neoSchema.getSchema().then((schema) => {
-  const server = new ApolloServer({
-    schema: schema,
-  });
-
-  server.listen().then(({ url }) => {
-    console.log(`GraphQL server ready on ${url}`);
-  });
+const neoSchema = new Neo4jGraphQL({
+  typeDefs,
+  driver,
 });
+
+const schema = neoSchema.getSchema().then((schema) => schema);
+const server = new ApolloServer({
+  schema: schema,
+});
+(async() => {
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async ({ req }) => ({
+      token: req.headers.authorization,
+    }),
+  });
+})()
+
